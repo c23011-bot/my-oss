f94282a9ead727c9a6ecf515deed0a30b1c1e6a8
suntech\c23011 <c23011@suntech.jp>
2026-06-29 15:40:14 +0900

    クラス名と返り値を修正しました

--- src/addLowMath.java
+++ src/addLowMath.java
@@ -1,5 +1,5 @@
-public class addLowMathClass{
-    public int calc(int a,int b){
-        System.out.println("入力された数字を計算します\n足し算です"+(a+b)+"引き算です"+(a-b) );
+public class Simple_Math{
+    public void simple_calc(int a,int b){
+        Sysvoid.out.println("入力された数字を計算します\n足し算です"+(a+b)+"引き算です"+(a-b) );
     }
 }
